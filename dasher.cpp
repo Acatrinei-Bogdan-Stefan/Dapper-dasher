589163baf0970955d78e56f476e0400c18c8944e
Acatrinei-Bogdan-Stefan <bogdan.z9.acatrinei@gmail.com>
2025-05-30 23:28:51 +0300

    Version 0.04

--- dasher.cpp
+++ dasher.cpp
@@ -33,20 +33,25 @@ Vector2 scarfyPos;
 scarfyPos.x = windowWidth/2 - scarfiRec.width/2;
 scarfyPos.y = windowHeight - scarfiRec.height;
 
+// animation frame
+int frame{};
+// amount of time before we update the animation frame
+const float updateTime{1.0/12.0};
+float runningTime{};
 
 // nebula variables
 Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
 Rectangle nebRec{0.0, 0.0, nebula.width/8, nebula.height/8};
 Vector2 nebPos{windowWidth, windowHeight- nebRec.height};
 
-int nebVel{-600};
+// nebula animation
+int nebFrame{};
+const float nebUpdateTime{1.0/12.0};
+float nebRunningTime{};
 
-// animation frame
-int frame{};
+//nebula x velocity(pixels/second)
+int nebVel{-100};
 
-// amount of time before we update the animation frame
-const float updateTime{1.0/12.0};
-float runningTime{};
 
 int velocity(0);
 
@@ -90,20 +95,40 @@ int velocity(0);
             if(!isInAir)
             {
 
-                //update running time
+                //update scarfi running time
                 runningTime += dT;
                 if(runningTime >= updateTime)
                 {
                     runningTime = 0.0;
                     //Update animation frame
-                    scarfiRec.x = frame * scarfiRec.width;
+                    scarfiRec.x = frame * scarfiRec.width;  
                     frame++;
+                    
                     if(frame > 5)
                     {
                      frame = 0;
                     }
-               }
+                }
             }
+
+
+            //update nebula running time
+                nebRunningTime += dT;
+                if(nebRunningTime >= nebUpdateTime)
+                {
+                    nebRunningTime = 0.0;
+
+                    //Update animation frame
+                    nebRec.x = nebFrame * nebRec.width;
+                    nebFrame++;
+                   
+                    if(nebFrame > 7)
+                    {
+                     nebFrame = 0;
+                    }
+
+                }
+            
 
 
            
